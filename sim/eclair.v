73dea7e0efc0ed2211c4a47e4485c5781ab6dcbe
Sen <github@sen.cx>
2012-12-16 14:49:28 -0500

    First real instruction (ldi8.al) works! ALU is partially integrated, full integration is the next step.

--- sim/eclair.v
+++ sim/eclair.v
@@ -22,7 +22,9 @@ module ECLair();
   wire          cs_ram__w;        // RAM control store write signal
   wire          cs_jump;          // control store jump signal
   wire  [7:0]   cs_jump_addr;     // control store jump destination
-  wire          alu_op;           // ALU operation
+  wire          alu_mode;         // ALU mode (0=arithmetic, 1=logic)
+  wire  [3:0]   alu_op;           // ALU operation
+  wire          mux_mdr_src;      // 0 = MDR sources from data, 1 = MDR sources from Z
   wire          ram__w;           // Main RAM write signal
   wire  [15:0]  pc;               // program counter
   wire  [7:0]   reg_load;         // load signals (latch clocks) for registers
@@ -32,6 +34,8 @@ module ECLair();
   wire          reg_d_load;
   wire          reg_ir_load;
   wire          reg_mar_load;
+  wire          reg_mdr_l_load;
+  wire          reg_mdr_h_load;
 //  wire  [15:0]  alu_x;            // alu I/O registers
 //  wire  [15:0]  alu_y;
   wire  [15:0]  alu_z;
@@ -48,10 +52,11 @@ module ECLair();
   wire  [7:0]   reg_ir;
   wire  [15:0]  reg_mar;
   wire  [15:0]  lat_mar;
+  wire  [15:0]  reg_mdr;
+  wire  [15:0]  lat_mdr;
   wire  [15:0]  lat_xy;
   
   initial begin
-    // set everything up
     clk_main = 1'b0;
     _ext_reset = 1'b1;
     _por_reset = 1'b0;
@@ -62,11 +67,11 @@ module ECLair();
   flipflop_jk                                   flp_cs_ready(.clk(clk_cs), .j(cs_addr[7]), .k(1'b0), .q(cs_ready), ._q());
   flipflop_jk                                   flp_clk_halver(clk_main, 1'b1, 1'b1, clk_half_a, clk_half_b);
   mux_21                                        mux_cs_clk_selector(cs_ready, clk_half_a, clk_main, clk_cs);
-  mux_28                                        mux_cs_jump_addr_src(cs_data[0], cs_data[9:2], reg_ir, cs_jump_addr);
+  mux_28                                        mux_cs_jump_addr_src(.sel(cs_data[0]), .a(reg_ir), .b(cs_data[9:2]), .y(cs_jump_addr));
   counter         #(.WIDTH(8))                  ctr_cs_seq(.clk(clk_cs), .reset(~_por_reset), .out(cs_addr), .load(cs_jump), .preset(cs_jump_addr));
   microcode_eprom #(.ROM_FILE("microcode.bin")) rom_cs(1'b0, 1'b0, cs_addr, cs_rom_data);
   microcode_ram                                 ram_cs(1'b0, 1'b0, cs_ram__w, cs_addr, cs_data_in, cs_data);
-  main_ram                                      ram_main(1'b0, addr_ram, ram__w, bus_addr[19:0], bus_data, bus_data);
+  main_ram                                      ram_main(._cs(1'b0), ._oe(addr_ram), ._w(ram__w), .addr(bus_addr[19:0]), .data_in(bus_data), .data_out(bus_data));
   main_eprom      #(.ROM_FILE("bootrom.bin"))   rom_boot(1'b0, addr_rom, bus_addr[19:0], bus_data);
   counter         #(.WIDTH(16))                 ctr_pc(.clk(cs_data[15]), .reset(~_reset), .out(pc), .load(cs_data[16]), .preset(reg_z));
   latch           #(.WIDTH(16))                 lat_reg_a(reg_a_load, reg_z, reg_a);
@@ -78,21 +83,29 @@ module ECLair();
   latch           #(.WIDTH(16))                 lat_reg_z(reg_z_load, alu_z, reg_z);
   latch                                         lat_reg_ir(reg_ir_load, bus_data, reg_ir);
   latch           #(.WIDTH(16))                 lat_reg_mar(reg_mar_load, lat_mar, reg_mar);
+  latch           #(.WIDTH(8))                  lat_reg_mdr_l(.clk(reg_mdr_l_load), .in(lat_mdr[7:0]),  .out(reg_mdr[7:0]));
+  latch           #(.WIDTH(8))                  lat_reg_mdr_h(.clk(reg_mdr_h_load), .in(lat_mdr[15:8]), .out(reg_mdr[15:8]));
   demux_38                                      dmx_reg_load(cs_data[19:17], reg_load);
-  mux_88                                        mux_xy_src_l(.sel(cs_data[28:26]), .a(8'b00000000), .y(lat_xy[7:0]));
-  mux_88                                        mux_xy_src_h(.sel(cs_data[28:26]), .a(8'b00000000), .y(lat_xy[15:8]));
-  mux_28                                        mux_mar_l(cs_data[10], pc[7:0], reg_z[7:0], lat_mar[7:0]);
-  mux_28                                        mux_mar_h(cs_data[10], pc[15:8], reg_z[15:8], lat_mar[15:8]);
-  alu                                           alu(clk_main, alu_op, reg_x, reg_y, reg_z);
+  mux_88                                        mux_xy_src_l(.sel(cs_data[28:26]), .a(8'b00000000), .b(reg_a[7:0]), .c(reg_b[7:0]), .d(reg_c[7:0]), .e(reg_d[7:0]), .h(reg_mdr[7:0]),  .y(lat_xy[7:0]));
+  mux_88                                        mux_xy_src_h(.sel(cs_data[28:26]), .a(8'b00000000), .b(reg_a[15:8]), .c(reg_b[15:8]), .d(reg_c[15:8]), .e(reg_d[15:8]), .h(reg_mdr[15:8]), .y(lat_xy[15:8]));
+  mux_28                                        mux_mar_l(.sel(cs_data[10]), .a(reg_z[7:0]),  .b(pc[7:0]),  .y(lat_mar[7:0]));
+  mux_28                                        mux_mar_h(.sel(cs_data[10]), .a(reg_z[15:8]), .b(pc[15:8]), .y(lat_mar[15:8]));
+  mux_28                                        mux_mdr_l(.sel(mux_mdr_src), .a(reg_z[7:0]),  .b(bus_data[7:0]), .y(lat_mdr[7:0]));
+  mux_28                                        mux_mdr_h(.sel(mux_mdr_src), .a(reg_z[15:8]), .b(bus_data[7:0]), .y(lat_mdr[15:8]));
+  alu_16                                        alu(.mode(alu_mode), .alu_op(alu_op), .c_in(1'b0), .x(reg_x), .y(reg_y), .z(alu_z));
   
   assign _reset = _ext_reset & _por_reset & cs_ready;
   assign cs_ram__w = cs_ready ~| clk_half_a;
   assign cs_jump = cs_data[1];
-  assign alu_op = cs_data[23:20];
+  assign alu_mode = cs_data[20];
+  assign alu_op = cs_data[24:21];
   assign reg_a_load = reg_load[0];
   assign reg_b_load = reg_load[1];
   assign reg_c_load = reg_load[2];
   assign reg_d_load = reg_load[3];
+  assign mux_mdr_src = cs_data[11];
+  assign reg_mdr_l_load = ~cs_data[12];
+  assign reg_mdr_h_load = ~cs_data[25];
   assign reg_mar_load = cs_data[13];
   assign reg_ir_load = cs_data[14];
   assign reg_x_load = cs_data[31];
@@ -115,6 +128,12 @@ module ECLair();
     $display("pc: 0x%06X", pc);
     $display("bus_addr: %0b", bus_addr);
     $display("bus_data: %0b (0x%0h)", bus_data, bus_data);
+    $display("reg_mar: %0b (0x%0h)", reg_mar, reg_mar);
+    $display("reg_mdr: %0b (0x%0h)", reg_mdr, reg_mdr);
+    $display("reg_a: %0b (0x%0h)", reg_a, reg_a);
+    $display("reg_b: %0b (0x%0h)", reg_b, reg_b);
+    $display("reg_c: %0b (0x%0h)", reg_c, reg_c);
+    $display("reg_d: %0b (0x%0h)", reg_d, reg_d);
     $display("reg_x: %0b (0x%0h)", reg_x, reg_x);
     $display("reg_y: %0b (0x%0h)", reg_y, reg_y);
     $display("reg_z: %0b (0x%0h)", reg_z, reg_z);
@@ -135,6 +154,8 @@ module ECLair();
   always @ (negedge cs_addr[7]) begin
     if(cs_ready) begin
       $display("Microcode loaded from ROM to RAM.");
+      $dumpfile("eclair.vcd");
+      $dumpvars(0,ECLair);
     end
   end
 endmodule
