c8c0dd8ef7890bd7627ac30cb820799695f9d851
Sen <github@sen.cx>
2012-12-16 17:17:16 -0500

    Fixing word length when chip is not selected.

--- sim/main_eprom.v
+++ sim/main_eprom.v
@@ -3,13 +3,13 @@ module main_eprom(_cs, _oe, addr, data);
   
   input           _cs;
   input           _oe;
-  input   [19:0]   addr;
+  input   [19:0]  addr;
   output  [7:0]  data; 
   reg     [7:0]  rom[0:1024];
   wire    [7:0]  data_val;
   
-  assign data_val = (!_cs) ? rom[addr] : 7'b0;
-  assign data = _oe ? 7'bZ : data_val;
+  assign data_val = (!_cs) ? rom[addr] : 8'b0;
+  assign data = _oe ? 8'bZ : data_val;
   
   initial begin
     $readmemb(ROM_FILE, rom);
--- sim/main_ram.v
+++ sim/main_ram.v
@@ -8,8 +8,8 @@ module main_ram(_cs, _oe, _w, addr, data_in, data_out);
   reg     [7:0]  ram[0:1024];
   wire    [7:0]  data_val;
   
-  assign data_val = (!_cs) ? ram[addr] : 7'bZ;
-  assign data_out = _oe ? 7'bZ : data_val;
+  assign data_val = (!_cs) ? ram[addr] : 8'bZ;
+  assign data_out = _oe ? 8'bZ : data_val;
   
   always @ (negedge _w) begin
     ram[addr] <= data_in;
