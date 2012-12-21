b9b18f2d02c246c10313052baa9c6edb7e13f3bf
Sen <github@sen.cx>
2012-12-21 11:37:01 -0500

    Changed all signals coming from the control store data to separate wires, to make renumbering easier.

--- sim/eclair.v
+++ sim/eclair.v
@@ -26,16 +26,25 @@ module ECLair();
   wire  [63:0]  cs_data;          // RAM control store, used to actually run the machine
   reg   [63:0]  cs_data_in;
   wire          cs_ram__w;        // RAM control store write signal
+  wire          cs_jump_src;      // control store jump source (0 = IR, 1 = MC bits)
+  wire  [7:0]   cs_jump_addr_mc;  // microcode bits used if cs_jump_src = 1
   wire          cs_jump;          // control store jump signal
   wire  [7:0]   cs_jump_addr;     // control store jump destination
+  wire          inc_pc;           // increment PC
+  wire          load_pc;          // load PC from Z
   wire          alu_mode;         // ALU mode (0=arithmetic, 1=logic)
   wire  [3:0]   alu_op;           // ALU operation
+  wire          ram_read;         // RAM read operation
+  wire          ram_write;        // RAM write operation
   wire          op_16bit;         // Operation is 16-bits wide
-  wire          mux_mdr_src;      // 0 = MDR sources from data, 1 = MDR sources from Z
+  wire          mux_mar_src;      // 0 = MAR sources from Z, 1 = MAR sources from PC
+  wire          mux_mdr_src;      // 0 = MDR sources from Z, 1 = MDR sources from data bus
   wire          ram__w;           // Main RAM write signal
   wire  [15:0]  pc;               // program counter
+  wire  [2:0]   load_reg;         // load general register
   wire  [7:0]   reg_load;         // load signals (latch clocks) for registers
   wire  [7:0]   reg_load_via_ir;  // load signals (latch clocks) for registers (when loading via IR)
+  wire  [2:0]   reg_xy_src;       // load source for X and Y registers
   wire  [2:0]   reg_x_load_ir_src;  // bits from IR to determine which register to load
   wire          reg_x_load_ir;    // load a register, getting the reg to load from IR[7..6]
   wire          reg_a_load;       
@@ -46,9 +55,6 @@ module ECLair();
   wire          reg_mar_load;
   wire          reg_mdr_l_load;
   wire          reg_mdr_h_load;
-//  wire  [15:0]  alu_x;            // alu I/O registers
-//  wire  [15:0]  alu_y;
-  wire  [15:0]  alu_z;
   wire          reg_x_load;
   wire          reg_y_load;
   wire          reg_z_load;
@@ -65,6 +71,7 @@ module ECLair();
   wire  [15:0]  reg_mdr;
   wire  [15:0]  lat_mdr;
   wire  [15:0]  lat_xy;
+  wire  [15:0]  alu_z;
   
   initial begin
     //$dumpfile("eclair.vcd");
@@ -79,13 +86,13 @@ module ECLair();
   counter         #(.WIDTH(3))                  ctr_clk_divider(.clk(clk_main), .ce(1'b1), .reset(1'b0), .out(clk_divided), .load(1'b0), .preset(3'b000));
   flipflop_jk                                   flp_cs_ready(.clk(top_of_cs), .j(1'b1), .k(1'b0), .q(cs_ready));
   mux_21                                        mux_cs_clk_selector(cs_ready, clk_quarter, clk_main, clk_cs);
-  mux_28                                        mux_cs_jump_addr_src(.sel(cs_data[0]), .a(reg_ir), .b(cs_data[9:2]), .y(cs_jump_addr));
+  mux_28                                        mux_cs_jump_addr_src(.sel(cs_jump_src), .a(reg_ir), .b(cs_jump_addr_mc), .y(cs_jump_addr));
   counter         #(.WIDTH(8))                  ctr_cs_seq(.clk(clk_cs), .ce(1'b1), .reset(~_por_reset), .out(cs_addr), .load(cs_jump & cs_ready), .preset(cs_jump_addr));
   microcode_eprom #(.ROM_FILE("microcode-full.bin")) rom_cs(1'b0, 1'b0, cs_addr, cs_rom_data);
   microcode_ram                                 ram_cs(1'b0, 1'b0, cs_ram__w, cs_addr, cs_data_in, cs_data);
   main_ram                                      ram_main(._cs(1'b0), ._oe(addr_ram), ._w(ram__w), .addr(bus_addr[19:0]), .data_in(bus_data), .data_out(bus_data));
   main_eprom      #(.ROM_FILE("bootrom.bin"))   rom_boot(1'b0, addr_rom, bus_addr[19:0], bus_data);
-  counter         #(.WIDTH(16))                 ctr_pc(.clk(cs_data[15]), .ce(1'b1), .reset(~_reset), .out(pc), .load(cs_data[16]), .preset(reg_z));
+  counter         #(.WIDTH(16))                 ctr_pc(.clk(inc_pc), .ce(1'b1), .reset(~_reset), .out(pc), .load(load_pc), .preset(reg_z));
   latch           #(.WIDTH(8))                  lat_reg_a_l(reg_a_load, reg_z[7:0], reg_a[7:0]);
   latch           #(.WIDTH(8))                  lat_reg_a_h(reg_a_load | ~op_16bit, reg_z[15:8], reg_a[15:8]);
   latch           #(.WIDTH(8))                  lat_reg_b_l(reg_b_load, reg_z[7:0], reg_b[7:0]);
@@ -101,15 +108,37 @@ module ECLair();
   latch           #(.WIDTH(16))                 lat_reg_mar(reg_mar_load, lat_mar, reg_mar);
   latch           #(.WIDTH(8))                  lat_reg_mdr_l(.clk(reg_mdr_l_load), .in(lat_mdr[7:0]),  .out(reg_mdr[7:0]));
   latch           #(.WIDTH(8))                  lat_reg_mdr_h(.clk(reg_mdr_h_load), .in(lat_mdr[15:8]), .out(reg_mdr[15:8]));
-  demux_38                                      dmx_reg_load(cs_data[19:17], reg_load);
+  demux_38                                      dmx_reg_load(load_reg, reg_load);
   demux_38                                      dmx_reg_load_ir(reg_x_load_ir_src, reg_load_via_ir);
-  mux_88                                        mux_xy_src_l(.sel(cs_data[28:26]), .a(16'b0000000000000000), .b(reg_a[7:0]), .c(reg_b[7:0]), .d(reg_c[7:0]), .e(reg_d[7:0]), .h(reg_mdr[7:0]),  .y(lat_xy[7:0]));
-  mux_88                                        mux_xy_src_h(.sel(cs_data[28:26]), .a(16'b0000000000000000), .b(reg_a[15:8]), .c(reg_b[15:8]), .d(reg_c[15:8]), .e(reg_d[15:8]), .h(reg_mdr[15:8]), .y(lat_xy[15:8]));
-  mux_28                                        mux_mar_l(.sel(cs_data[10]), .a(reg_z[7:0]),  .b(pc[7:0]),  .y(lat_mar[7:0]));
-  mux_28                                        mux_mar_h(.sel(cs_data[10]), .a(reg_z[15:8]), .b(pc[15:8]), .y(lat_mar[15:8]));
+  mux_88                                        mux_xy_src_l(.sel(reg_xy_src), .a(16'b0000000000000000), .b(reg_a[7:0]), .c(reg_b[7:0]), .d(reg_c[7:0]), .e(reg_d[7:0]), .h(reg_mdr[7:0]),  .y(lat_xy[7:0]));
+  mux_88                                        mux_xy_src_h(.sel(reg_xy_src), .a(16'b0000000000000000), .b(reg_a[15:8]), .c(reg_b[15:8]), .d(reg_c[15:8]), .e(reg_d[15:8]), .h(reg_mdr[15:8]), .y(lat_xy[15:8]));
+  mux_28                                        mux_mar_l(.sel(mux_mar_src), .a(reg_z[7:0]),  .b(pc[7:0]),  .y(lat_mar[7:0]));
+  mux_28                                        mux_mar_h(.sel(mux_mar_src), .a(reg_z[15:8]), .b(pc[15:8]), .y(lat_mar[15:8]));
   mux_28                                        mux_mdr_l(.sel(mux_mdr_src), .a(reg_z[7:0]),  .b(bus_data[7:0]), .y(lat_mdr[7:0]));
   mux_28                                        mux_mdr_h(.sel(mux_mdr_src), .a(reg_z[15:8]), .b(bus_data[7:0]), .y(lat_mdr[15:8]));
   alu_16                                        alu(.mode(alu_mode), .alu_op(alu_op), .c_in(1'b0), .x(reg_x), .y(reg_y), .z(alu_z));
+  
+  assign cs_jump_src = cs_data[0];
+  assign #1 cs_jump = cs_data[1];
+  assign cs_jump_addr_mc = cs_data[9:2];
+  assign mux_mar_src = cs_data[10];
+  assign mux_mdr_src = cs_data[11];
+  assign reg_mdr_l_load = ~cs_data[12];
+  assign reg_mar_load = ~cs_data[13];
+  assign reg_ir_load = ~cs_data[14];
+  assign inc_pc = cs_data[15];
+  assign load_pc = cs_data[16];
+  assign load_reg = cs_data[19:17];
+  assign alu_mode = cs_data[20];
+  assign alu_op = cs_data[24:21];
+  assign reg_mdr_h_load = ~cs_data[25];
+  assign reg_xy_src = cs_data[28:26];
+  assign ram_read = cs_data[29];
+  assign ram_write = cs_data[30];
+  assign reg_x_load = ~cs_data[31];
+  assign reg_y_load = ~cs_data[32];
+  assign reg_z_load = ~cs_data[33];
+  assign op_16bit = cs_data[34];
   
   assign clk_half = clk_divided[1];
   assign clk_quarter = clk_divided[2];
@@ -118,23 +147,11 @@ module ECLair();
   assign processor_halted = cs_ready & cs_addr == 8'hFE;
   assign _reset = _ext_reset & _por_reset & cs_ready;
   assign cs_ram__w = cs_ready ~| clk_cs;
-  assign #1 cs_jump = cs_data[1];
-  assign alu_mode = cs_data[20];
-  assign alu_op = cs_data[24:21];
-  assign op_16bit = cs_data[34];
   assign reg_a_load = reg_load[1] & reg_load_via_ir[1];
   assign reg_b_load = reg_load[2] & reg_load_via_ir[3];
   assign reg_c_load = reg_load[3] & reg_load_via_ir[5];
   assign reg_d_load = reg_load[4] & reg_load_via_ir[7];
   assign reg_x_load_ir = ~reg_load[7];
-  assign mux_mdr_src = cs_data[11];
-  assign reg_mdr_l_load = ~cs_data[12];
-  assign reg_mdr_h_load = ~cs_data[25];
-  assign reg_mar_load = ~cs_data[13];
-  assign reg_ir_load = ~cs_data[14];
-  assign reg_x_load = ~cs_data[31];
-  assign reg_y_load = ~cs_data[32];
-  assign reg_z_load = ~cs_data[33];
   assign addr_rom = ~(bus_addr[23:20] == 4'b0000);
   assign addr_device = ~(bus_addr[23:20] == 4'b0111);
   assign addr_ram = ~(addr_rom ~| addr_device);
