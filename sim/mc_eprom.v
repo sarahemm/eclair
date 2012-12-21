81bcf8cd7f8d6fa3618e0dbf8a82d0a729cc69e7
Sen <github@sen.cx>
2012-12-21 16:57:01 -0500

    Parameterized width.

--- sim/mc_eprom.v
+++ sim/mc_eprom.v
@@ -2,13 +2,14 @@
 
 module microcode_eprom(_cs, _oe, addr, data);
   parameter ROM_FILE="eprom.bin";
+  parameter WIDTH=64;
   
-  input           _cs;
-  input           _oe;
-  input   [7:0]   addr;
-  output  [63:0]  data; 
-  reg     [63:0]  rom[0:511];
-  wire    [63:0]  data_val;
+  input                 _cs;
+  input                 _oe;
+  input   [7:0]         addr;
+  output  [WIDTH-1:0]   data; 
+  reg     [WIDTH-1:0]   rom[0:511];
+  wire    [WIDTH-1:0]   data_val;
   
   assign #40 data_val = (!_cs) ? rom[addr] : 64'b0;
   assign #40 data = _oe ? 64'bZ : data_val;
--- sim/mc_ram.v
+++ sim/mc_ram.v
@@ -1,14 +1,16 @@
 `timescale 1ns/1ps
 
 module microcode_ram(_cs, _oe, _w, addr, data_in, data_out);
+  parameter WIDTH=64;
+
   input           _cs;
   input           _oe;
   input           _w;
   input   [7:0]   addr;
-  input   [63:0]  data_in;
-  output  [63:0]  data_out;
-  reg     [63:0]  ram[0:511];
-  wire    [63:0]  data_val;
+  input   [WIDTH-1:0]  data_in;
+  output  [WIDTH-1:0]  data_out;
+  reg     [WIDTH-1:0]  ram[0:511];
+  wire    [WIDTH-1:0]  data_val;
   
   assign #5 data_val = (!_cs) ? ram[addr] : 64'b0;
   assign data_out = _oe ? 64'bZ : data_val;
