f05de92c8d628887e03f00765e8ef5e077fc5988
Sen <github@sen.cx>
2012-12-21 19:58:57 -0500

    Adding missing D flipflop model.

---
+++ sim/flipflop_d.v
@@ -0,0 +1,13 @@
+`timescale 1ns/1ps
+
+module flipflop_d(clk, reset, in, out);
+  parameter WIDTH=8;
+  input                   clk;
+  input                   reset;
+  input       [WIDTH-1:0] in;
+  output reg  [WIDTH-1:0] out;
+  
+  always @ (posedge clk or posedge reset) begin
+    #0.5 out <= in & ~reset;
+  end
+endmodule
