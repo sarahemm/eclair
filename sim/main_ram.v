8c08ac0c35d0503f0a789560fcffcba4a39d8a7e
Sen <github@sen.cx>
2012-12-25 18:49:29 -0500

    Parameterized RAM width.

--- sim/main_ram.v
+++ sim/main_ram.v
@@ -1,17 +1,19 @@
 `timescale 1ns/1ps
 
 module main_ram(_cs, _oe, _w, addr, data_in, data_out);
-  input           _cs;
-  input           _oe;
-  input           _w;
-  input   [19:0]   addr;
-  input   [7:0]  data_in;
-  output  [7:0]  data_out;
-  reg     [7:0]  ram[0:1024];
-  wire    [7:0]  data_val;
+  parameter WIDTH=8;
   
-  assign #5 data_val = (!_cs) ? ram[addr] : 8'bZ;
-  assign data_out = _oe ? 8'bZ : data_val;
+  input               _cs;
+  input               _oe;
+  input               _w;
+  input   [19:0]      addr;
+  input   [WIDTH-1:0] data_in;
+  output  [WIDTH-1:0] data_out;
+  reg     [WIDTH-1:0] ram[0:1024];
+  wire    [WIDTH-1:0] data_val;
+  
+  assign #5 data_val = (!_cs) ? ram[addr] : 16'bZ;
+  assign data_out = _oe ? 16'bZ : data_val;
   
   always @ (negedge _w) begin
     ram[addr] <= data_in;
