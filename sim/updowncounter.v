ed90b561286466250a4bc11a29bea68bc1a4cf5b
sarahemm <github@sen.cx>
2018-04-22 23:02:04 -0400

    Added carry output support for decrement operations.

--- sim/updowncounter.v
+++ sim/updowncounter.v
@@ -1,9 +1,11 @@
 `timescale 1ns/1ps
 
 // sync-load async-reset up/down counter (like MC10E136)
-module updowncounter(clk, reset, out, preset, mode);
+module updowncounter(clk, reset, out, preset, mode, cout, clout);
   parameter WIDTH=8;
   output  reg [WIDTH-1:0] out;
+  output  reg             cout;
+  output  reg             clout;
   input                   clk;
   input                   reset;
   input                   load;
@@ -17,17 +19,22 @@ module updowncounter(clk, reset, out, preset, mode);
   always @(posedge clk or posedge reset) begin
     $display("udcounter: c%0b r%0b l%0b", clk, reset, load);
     if (reset) begin
+      // reset
       out <= #1.25 0;
-      $display("udcounter: reset");
+      cout <= 0;
+      clout <= 1;
     end else if(mode == 2'b00) begin
+      // load
       out <= #1.25 preset;
-      $display("udcounter: load");
     end else if(mode == 2'b01) begin
-      out <= #1.25 out - 1;
-      $display("udcounter: count up");
+      // decrement
+      clout <= #1.25 out == 2;
+      cout  <= #1.25 out == 1;
+      out   <= #1.25 out - 1;
     end else if(mode == 2'b10) begin
+      // increment
+      // TODO: support carry outputs with increment like we do with decrement
       out <= #1.25 out + 1;
-      $display("udcounter: count up");
     end
   end
 endmodule 
