9e47c45e5c642d7bdf80518f89d1504c98a55f01
sarahemm <github@sen.cx>
2019-04-12 19:00:38 -0400

    Converted shr16 tests to llvm-mc format.

--- sim/test/shr16.a.test.s
+++ sim/test/shr16.a.test.s
@@ -1,13 +1,12 @@
-@000
-ldi16.a 0b0001000100010001
+ldi16 a, 0b0001000100010001
 cir
-shr16.a 0x04
+shr16 a, 0x04
 // expect: reg_a=00000001_00010001
 // expect: reg_b=xxxxxxxx_xxxxxxxx
 // expect: reg_c=xxxxxxxx_xxxxxxxx
 // expect: reg_d=xxxxxxxx_xxxxxxxx
 cir
-shr16.a 0x02
+shr16 a, 0x02
 halt
 // expect: reg_a=00000000_01000100
 // expect: reg_b=xxxxxxxx_xxxxxxxx
--- sim/test/shr16.c.test.s
+++ sim/test/shr16.c.test.s
@@ -1,13 +1,12 @@
-@000
-ldi16.c 0b0001000100010001
+ldi16 c, 0b0001000100010001
 cir
-shr16.c 0x04
+shr16 c, 0x04
 // expect: reg_a=xxxxxxxx_xxxxxxxx
 // expect: reg_b=xxxxxxxx_xxxxxxxx
 // expect: reg_c=00000001_00010001
 // expect: reg_d=xxxxxxxx_xxxxxxxx
 cir
-shr16.c 0x02
+shr16 c, 0x02
 halt
 // expect: reg_a=xxxxxxxx_xxxxxxxx
 // expect: reg_b=xxxxxxxx_xxxxxxxx
