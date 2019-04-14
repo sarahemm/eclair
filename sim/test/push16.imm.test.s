7d0a3a742112075279983e80357545132a9469a4
sarahemm <github@sen.cx>
2019-04-14 15:36:58 -0400

    Converted push16.imm test to llvm-mc format.

--- sim/test/push16.imm.test.s
+++ sim/test/push16.imm.test.s
@@ -1,25 +1,24 @@
-@000
-ldi8.flags  0x00    // system mode, paging off
+ldi flags, 0x00    // system mode, paging off
 // set up the system page table (so we can get to RAM)
 // page table address 0 = 00000000_00000000 (first block of ROM)
-@002
-ldi16.a     0x0000
-ldi16.d     0x0000
+.org 0x002
+ldi a,     0x0000
+ldi d,     0x0000
 wrpte
 // page table address 1 = 00000100_00000000 (first block of RAM)
-@009
-ldi16.a     0x0400
-ldi16.d     0x0401
+.org 0x009
+ldi a,     0x0400
+ldi d,     0x0401
 wrpte
 // set up the stack pointer to the bottom of RAM and enable paging
-@010
-ldi16.sp    0x0400
-ldi8.flags  0x05    // system mode, paging on
-@015
-push16.imm  0xAA55
-@018
-push16.imm  0x0EC2
-@01B
+.org 0x010
+ldi sp,    0x0400
+ldi flags, 0x05    // system mode, paging on
+.org 0x015
+push16i   0xAA55
+.org 0x018
+push16i   0x0EC2
+.org 0x01B
 halt
 
 // expect: pc=0x00001b
