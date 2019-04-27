0cb5b276a31a23d76b97c61e55e2538a2c4284c3
sarahemm <github@sen.cx>
2019-04-27 11:13:18 -0400

    push8 test converted to llvm-mc format.

--- sim/test/push8.imm.test.s
+++ sim/test/push8.imm.test.s
@@ -1,23 +1,22 @@
-@000
-ldi8.flags  0x00    // system mode, paging off
+ldi flags,  0x00    // system mode, paging off
 // set up the system page table (so we can get to RAM)
 // page table address 0 = 00000000_00000000 (first block of ROM)
-@002
-ldi16.a     0x0000
-ldi16.d     0x0000
+.org 0x002
+ldi a,     0x0000
+ldi d,     0x0000
 wrpte
 // page table address 1 = 00000100_00000000 (first block of RAM)
-@009
-ldi16.a     0x0400
-ldi16.d     0x0401
+.org 0x009
+ldi a,     0x0400
+ldi d,     0x0401
 wrpte
 // set up the stack pointer to the bottom of RAM and enable paging
-@010
-ldi16.sp    0x0400
-ldi8.flags  0x05    // system mode, paging on
-@015
-push8.imm   0x55
-push8.imm   0xAA
+.org 0x010
+ldi sp,    0x0400
+ldi flags, 0x05    // system mode, paging on
+.org 0x015
+push8i     0x55
+push8i     0xAA
 halt
 
 // expect: pc=0x000019
