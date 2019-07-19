a4bd373002ace7420966034bef371c3cd689cfb3
sarahemm <github@sen.cx>
2019-07-19 11:30:21 -0400

    Added alignment checking.
    rptz_next_addr can only jump within the same 16-instruction block as
    it's only a 4-bit-wide field, so this checks to ensure this constraint
    isn't violated.

--- utils/mcgen/fielddecode-rptznext.rb
+++ utils/mcgen/fielddecode-rptznext.rb
@@ -1,28 +1,37 @@
+# rptz_next_addr values are actually locations to jump to, so find that
 class FieldDecodeRptzNext
   def decode(field: nil, value: nil, addr: nil)
     return nil unless field == 'rptz_next_addr'
 
     locations = Locations.instance
+    rptz_next_addr = nil
 
-    # rptz_next_addr values are actually locations to jump to, so find that
-    # also these only use the least significant 5 bits
     # FIXME: add alignment checking
-    case value
+    rptz_next_addr = case value
       when 'ir'
-        return 0
+        0
       when '-1'
-        return (addr - 1) & 0x0F
+        (addr - 1)
       when '0'
-        return addr & 0x0F
+        addr & 0x0F
       when '+1'
-        return (addr + 1) & 0x0F
+        (addr + 1)
       when '+2'
-        return (addr + 2) & 0x0F
+        (addr + 2)
       else
         location = locations[value]
         raise ArgumentError, "No such rptz_next_addr '#{value}'" unless location
 
-        return location.address & 0x0F
+        location.address
+      end
+
+    # rptz_next_addr can only jump within the same 16-instruction block
+    # make sure this new address falls within that
+    if rptz_next_addr.to_s(2)[0..-5] != addr.to_s(2)[0..-5]
+      raise ArgumentError, "RPTZ Next Addr can only jump within the same 16-instruction block, realignment required (#{value}/#{rptz_next_addr.to_s(2).rjust(9, '0')} vs #{addr.to_s(2).rjust(9, '0')})"
     end
+
+    # rptz_next_addr only uses the least significant 4 bits
+    rptz_next_addr & 0x1F
   end
 end
