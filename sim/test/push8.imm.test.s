2679470517df472d36953b7c3fdeb0ce63d885ee
sarahemm <github@sen.cx>
2019-09-26 17:57:29 -0400

    Fixing page flags for a number of tests.
    Paging setup for these now properly sets present and writable bits in the page table.

--- sim/test/push8.imm.test.s
+++ sim/test/push8.imm.test.s
@@ -2,12 +2,12 @@ ldi flags,  0x00    // system mode, paging off
 // set up the system page table (so we can get to RAM)
 // page table address 0 = 00000000_00000000 (first block of ROM)
 .org 0x002
-ldi a,     0x0000
+ldi a,     0x8000
 ldi d,     0x0000
 wrpte
 // page table address 1 = 00000100_00000000 (first block of RAM)
 .org 0x009
-ldi a,     0x0400
+ldi a,     0xC400
 ldi d,     0x0401
 wrpte
 // set up the stack pointer to the bottom of RAM and enable paging
