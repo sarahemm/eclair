6f064c6e55ca1e416623ce204ae1e6022348d9c3
sarahemm <github@sen.cx>
2020-11-14 14:52:22 -0500

    Added instruction to push 8 high bits of a register to the stack.

---
+++ sim/test/push8.dh.test.s
@@ -0,0 +1,26 @@
+ldi flags,  0x00  // system mode, paging off
+// set up the system page table (so we can get to RAM)
+// page table address 0 = 00000000_00000000 (first block of ROM)
+.org 0x002
+ldi a,     0x8000
+ldi d,     0x0000
+wrpte
+// page table address 1 = 00000100_00000000 (first block of RAM)
+.org 0x009
+ldi a,     0xC400
+ldi d,     0x0401
+wrpte
+// set up the stack pointer to the bottom of RAM and enable paging
+.org 0x010
+ldi sp,    0x0400
+ldi flags, 0x05  // system mode, paging on
+.org 0x015
+// load a test value into high byte of d
+ldi dh, 0xAE
+push dh
+.org 0x018
+halt
+
+// expect: pc=0x000018
+// expect: reg_sp=00000100_00000001
+// expect: ram[0x100000]=0xae
