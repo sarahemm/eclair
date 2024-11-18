17a097682eb8b831bf5aad2335c3303d0f25fb65
sen <github@sen.cx>
2024-11-17 20:24:55 -0500

    Fixed ld16 to now set Z status, and updated tests
    Now ld16 tests check for both Z states, so if there's a regression
    it'll be caught now. Still need to update ld8 for the same.

--- sim/unit_tests/ld16.b.test.s
+++ sim/unit_tests/ld16.b.test.s
@@ -1,17 +1,27 @@
 ldi dp, 0x0000
 cir
-ld  b,  0x000f
+ld  b,  0x0012
 // expect: reg_a=xxxxxxxx_xxxxxxxx
 // expect: reg_b=00010001_00010010
 // expect: reg_c=xxxxxxxx_xxxxxxxx
 // expect: reg_d=xxxxxxxx_xxxxxxxx
+// expect: status=XXXXXXX0
 ldi dp, 0x0002
 cir
-ld  b,  0x000f
-halt
+ld  b,  0x0012
+// expect: reg_a=xxxxxxxx_xxxxxxxx
 // expect: reg_b=11011110_11011111
-// expect: pc=0x00000e
+// expect: reg_c=xxxxxxxx_xxxxxxxx
+// expect: reg_d=xxxxxxxx_xxxxxxxx
+// expect: status=XXXXXXX0
+ld  b,  0x0014
+// expect: reg_b=00000000_00000000
+// expect: status=XXXXXXX1
+// expect: pc=0x000010
+halt
 .data 0x11
 .data 0x12
 .data 0xDE
 .data 0xDF
+.data 0x00
+.data 0x00
--- sim/unit_tests/ld16.d.test.s
+++ sim/unit_tests/ld16.d.test.s
@@ -1,17 +1,27 @@
 ldi dp, 0x0000
 cir
-ld  d,  0x000f
+ld  d,  0x0012
 // expect: reg_a=xxxxxxxx_xxxxxxxx
 // expect: reg_b=xxxxxxxx_xxxxxxxx
 // expect: reg_c=xxxxxxxx_xxxxxxxx
 // expect: reg_d=00010001_00010010
+// expect: status=XXXXXXX0
 ldi dp, 0x0002
 cir
-ld  d,  0x000f
-halt
+ld  d,  0x0012
+// expect: reg_a=xxxxxxxx_xxxxxxxx
+// expect: reg_b=xxxxxxxx_xxxxxxxx
+// expect: reg_c=xxxxxxxx_xxxxxxxx
 // expect: reg_d=11011110_11011111
-// expect: pc=0x00000e
+// expect: status=XXXXXXX0
+ld  d,  0x0014
+// expect: reg_d=00000000_00000000
+// expect: status=XXXXXXX1
+// expect: pc=0x000010
+halt
 .data 0x11
 .data 0x12
 .data 0xDE
 .data 0xDF
+.data 0x00
+.data 0x00
